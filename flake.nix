86afff1cbcc5906b1f4215e964ce7c6f2e78a11e
Vanilla <osu_Vanilla@126.com>
2022-04-21 16:09:38 +0800

    style[flake.nix]: system = "x86_64-linux";

--- flake.nix
+++ flake.nix
@@ -4,9 +4,11 @@
   };
 
   outputs = { self, ... }@inputs: with inputs; rec {
+    system = "x86_64-linux";
+
     azure-image = azure.config.system.build.azureImage;
-    azure = nixpkgs.lib.nixosSystem rec {
-      system = "x86_64-linux";
+    azure = nixpkgs.lib.nixosSystem {
+      inherit system;
       specialArgs = { inherit nixpkgs; };
       modules = [ ./nixos/configuration.nix ];
     };
