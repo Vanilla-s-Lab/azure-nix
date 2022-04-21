bdc91bc69fe83cfbba53849ce910cc4e3a07c8b8
Vanilla <osu_Vanilla@126.com>
2022-04-21 14:17:13 +0800

    feat[main.tf]: Add azuread 2.20.0, setting up application for tf cloud.

--- main.tf
+++ main.tf
@@ -4,11 +4,39 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.2.0"
     }
+
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.20.0"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
+}
+
+provider "azuread" {}
+
+resource "azuread_application" "example" {
+  display_name = "example"
+}
+
+resource "azuread_application_password" "example" {
+  application_object_id = azuread_application.example.object_id
+}
+
+resource "azuread_service_principal" "example" {
+  application_id = azuread_application.example.application_id
+}
+
+data "azurerm_subscription" "Student" {}
+
+# https://github.com/hashicorp/terraform-provider-azuread/issues/40
+resource "azurerm_role_assignment" "main" {
+  scope                = data.azurerm_subscription.Student.id
+  role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.example.id
 }
 
 resource "azurerm_resource_group" "NixOS" {
