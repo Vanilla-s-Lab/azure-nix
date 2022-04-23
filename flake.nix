43d01ab496f88427c9020424f42cb2c4f3828289
Vanilla <osu_Vanilla@126.com>
2022-04-23 20:51:49 +0800

    feat[flake.nix]: With deploy-rs, apply for 20.239.149.117 azure.

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
   inputs = {
     # https://github.com/NixOS/nixpkgs/issues/93032
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    deploy-rs.url = "github:serokell/deploy-rs";
   };
 
-  outputs = { self, nixpkgs, ... }: rec {
+  outputs = { self, nixpkgs, deploy-rs, ... }: rec {
     system = "x86_64-linux";
 
     azure-image = azure.config.system.build.azureImage;
@@ -18,6 +19,17 @@
       name = "Terraform_Azure";
       nativeBuildInputs = with pkgs;
         [ terraform azure-cli azure-storage-azcopy gh ];
+    };
+
+    deploy.nodes.azure = {
+      sshUser = "root";
+      hostname = "20.239.149.117";
+
+      profiles.system.path =
+        deploy-rs.lib."${system}".activate.nixos
+          azure;
+
+      fastConnection = true;
     };
   };
 }
