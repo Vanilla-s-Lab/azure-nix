d66d1a77abeea811c9009deb416398f01fe1b037
Vanilla <osu_Vanilla@126.com>
2022-04-23 21:52:40 +0800

    feat[nixos/configuration.nix]: Enable TFO server mode.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,4 +2,7 @@
 {
   imports = [ "${modulesPath}/virtualisation/azure-image.nix" ]
     ++ [ ./users.nix ./network.nix ./packages.nix ];
+
+  # https://sysctl-explorer.net/net/ipv4/tcp_fastopen/
+  boot.kernel.sysctl = { "net.ipv4.tcp_fastopen" = 2; };
 }
