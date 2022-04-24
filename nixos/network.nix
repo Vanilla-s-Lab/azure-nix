43c2586adacd7befaa76e69ee39d3e5e06faf65d
Vanilla <osu_Vanilla@126.com>
2022-04-24 15:01:21 +0800

    feat[nixos/network.nix]: Enable all recommanded options.

--- nixos/network.nix
+++ nixos/network.nix
@@ -12,4 +12,10 @@
 
   security.acme.acceptTerms = true;
   security.acme.email = "osu_Vanilla@126.com";
+
+  # services.nginx.recommended*
+  services.nginx.recommendedTlsSettings = true;
+  services.nginx.recommendedProxySettings = true;
+  services.nginx.recommendedOptimisation = true;
+  services.nginx.recommendedGzipSettings = true;
 }
