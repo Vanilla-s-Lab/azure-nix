8562f458396362adbdc039d15d190fc5fe26edbb
Vanilla <osu_Vanilla@126.com>
2022-04-29 17:21:17 +0800

    feat[nixos/nginx.nix]: Enable Nginx forward to api.bilibili.com.

--- nixos/nginx.nix
+++ nixos/nginx.nix
@@ -1,10 +1,11 @@
 { ... }:
 {
   services.nginx.enable = true;
-  services.nginx.virtualHosts."vanilla.scp.link" = {
-    addSSL = true;
-    enableACME = true;
-  };
+  services.nginx.virtualHosts."vanilla.scp.link" =
+    ({ addSSL = true; enableACME = true; } // {
+      # https://github.com/yujincheng08/BiliRoaming/wiki/%E8%87%AA%E5%BB%BA%E8%A7%A3%E6%9E%90%E6%9C%8D%E5%8A%A1%E5%99%A8
+      locations."/pgc/player/api/playurl".proxyPass = "https://api.bilibili.com";
+    });
 
   security.acme.email = "osu_Vanilla@126.com";
   security.acme.acceptTerms = true;
