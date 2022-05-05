e124b75e8304f4fa94e5363f431699ff5eafef88
Vanilla <osu_Vanilla@126.com>
2022-05-05 21:07:07 +0800

    update[global.tf]: azurerm 3.3.0 -> 3.4.0, azuread 2.21.0 -> 2.22.0.

--- global.tf
+++ global.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.3.0"
+      version = "3.4.0"
     }
 
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.21.0"
+      version = "2.22.0"
     }
   }
 
