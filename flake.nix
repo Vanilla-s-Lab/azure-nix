958949365f35fc8f37f7cd1f2c6d37b11a322cb7
Vanilla <osu_Vanilla@126.com>
2022-05-29 11:57:51 +0800

    style[flake.nix]: Remove devShell & .envrc.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # https://github.com/serokell/deploy-rs
     deploy-rs.url = "github:serokell/deploy-rs";
@@ -15,7 +14,7 @@
     nixos-cn.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, deploy-rs, nixpkgs-unstable, sops-nix, nixos-cn, ... }: rec {
+  outputs = { self, nixpkgs, deploy-rs, sops-nix, nixos-cn, ... }: rec {
     system = "x86_64-linux";
 
     azure-image = azure.config.system.build.azureImage;
@@ -24,15 +23,6 @@
       modules = [ ./nixos/configuration.nix ]
         ++ [ sops-nix.nixosModules.sops ]
         ++ [ nixos-cn.nixosModules.nixos-cn ];
-    };
-
-    pkgs = import nixpkgs { inherit system; };
-    pkgsUnstable = import nixpkgs-unstable { inherit system; };
-
-    devShell."${system}" = pkgs.mkShell {
-      name = "Terraform_Azure";
-      nativeBuildInputs = (with pkgsUnstable;
-        [ terraform azure-storage-azcopy gh azure-cli ]);
     };
 
     deploy.nodes.azure = {
