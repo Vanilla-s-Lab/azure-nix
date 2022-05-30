348376c31f8576a14f689360212b41871b6d147e
Vanilla <osu_Vanilla@126.com>
2022-05-30 11:06:58 +0800

    feat[nixos/nginx.nix]: addSSL -> onlySSL, close port 80.

--- nixos/nginx.nix
+++ nixos/nginx.nix
@@ -3,7 +3,7 @@
   services.nginx.enable = true;
   services.nginx.package = pkgs.nginxQuic;
   services.nginx.virtualHosts."vanilla.scp.link" =
-    ({ addSSL = true; enableACME = true; } // {
+    ({ /* addSSL = true; */ onlySSL = true; enableACME = true; } // {
       # https://github.com/yujincheng08/BiliRoaming/wiki/%E8%87%AA%E5%BB%BA%E8%A7%A3%E6%9E%90%E6%9C%8D%E5%8A%A1%E5%99%A8
       locations."/pgc/player/api/playurl".proxyPass = "https://api.bilibili.com";
     } // { http2 = true; http3 = true; });
@@ -16,5 +16,5 @@
   security.acme.defaults.email = "osu_Vanilla@126.com";
   security.acme.acceptTerms = true;
 
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
+  networking.firewall.allowedTCPPorts = [ 443 ];
 }
