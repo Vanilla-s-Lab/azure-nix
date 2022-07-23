d435d676d1c069a377710f0b35e583bf28d77084
Vanilla <osu_Vanilla@126.com>
2022-07-23 16:36:26 +0800

    feat[nixos/nginx.nix]: Remove bilibili api proxy.

--- nixos/nginx.nix
+++ nixos/nginx.nix
@@ -3,13 +3,8 @@
   services.nginx.enable = true;
   services.nginx.package = pkgs.nginxQuic;
   services.nginx.virtualHosts."vanilla.scp.link" =
-    ({ addSSL = true; /* onlySSL = true; */ enableACME = true; } // {
-      # https://github.com/yujincheng08/BiliRoaming/wiki/%E8%87%AA%E5%BB%BA%E8%A7%A3%E6%9E%90%E6%9C%8D%E5%8A%A1%E5%99%A8
-      locations."/pgc/player/api/playurl".proxyPass = "https://api.bilibili.com";
-    } // { http2 = true; http3 = true; } // {
-      # https://www.nginx.com/blog/improving-nginx-performance-with-kernel-tls/
-      kTLS = true;
-    });
+    ({ addSSL = true; /* onlySSL = true; */ enableACME = true; }
+      // { http2 = true; http3 = true; kTLS = true; });
 
   services.nginx.recommendedOptimisation = true;
   services.nginx.recommendedTlsSettings = true;
